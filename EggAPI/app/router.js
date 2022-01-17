5a3072e70fd19318e0414c04a1dbf1d2611c9267
suchcl <ya5969@163.com>
2022-01-17 13:38:50 +0800

    代码格式化

--- EggAPI/app/router.js
+++ EggAPI/app/router.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get("/", controller.home.index);
 
-  router.get('/getUser', controller.home.getUser);
-  router.get('/getProvince', controller.home.getProvince);
-  router.post('/addUser', controller.home.addUser);
+  router.get("/getUser", controller.home.getUser);
+  router.get("/getProvince", controller.home.getProvince);
+  router.post("/addUser", controller.home.addUser);
 };
